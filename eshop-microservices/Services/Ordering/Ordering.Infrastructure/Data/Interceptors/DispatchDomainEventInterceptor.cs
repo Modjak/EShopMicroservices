b59faf056cbb33d82864541469b94ccfbfe92ae7
Mocak <75246899+Modjak@users.noreply.github.com>
2024-08-08 18:25:43 +0300

    Add Domain Event Handlers

---
+++ eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/DispatchDomainEventInterceptor.cs
@@ -0,0 +1,29 @@
+﻿//using Microsoft.EntityFrameworkCore.Diagnostics;
+
+//namespace Ordering.Infrastructure.Data.Interceptors;
+//public class DispatchDomainEventInterceptor : SaveChangesInterceptor
+//{
+//    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+//    {
+//        DispatchDomainevent(eventData.Context).GetAwaiter().GetResult()
+//        return base.SavingChanges(eventData, result);
+//    }
+
+//    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+//    {
+//        await DispatchDomainevent(eventData.Context);
+//        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+//    }
+//    private async Task DispatchDomainevent(DbContext? context)
+//    {
+//        if(context == null)
+//        {
+//            return;
+//        }
+
+//        var aggregates = context.ChangeTracker
+//            .Entries<IAggregate>()
+//            .Where(a => a.Entity.DomainEvents.Any())
+//            .Select(a => a.Entity);
+//    }
+//}
